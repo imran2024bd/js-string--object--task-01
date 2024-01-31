008bb7d7a3716a93596bb0cefda2c29e94913e26
imran2024bd <imranitbd34@gmail.com>
2024-01-31 13:19:31 +0600

    Reversing step by for loop

--- string.js
+++ string.js
@@ -8,12 +8,48 @@
 // \\	\	Backslash
 
 
-const country = 'bangledesh';
-console.log(country);
-const address = '712 22 3';
-const capital = 'dhaka';
-const district = 'Natore';
-const sentence = 'i love my country';
+// const country = 'bangledesh';
+// // console.log(country);
+// const address = '712 22 3';
+// const capital = 'dhaka';
+// const district = 'Natore';
+// const sentence = 'i love my country';
 
-console.log(country.toUpperCase);
+// console.log(country.toUpperCase);
+
+///*************************************************************************************** */
+// reverse result = " .yrtnuoc ym ...............";
+
+// const sentence = 'i love my country.';
+// let reverse = '';
+// for ( const letter of sentence) {
+//     // console.log(letter);
+//     reverse = letter + reverse;
+    
+// }
+
+// console.log(reverse);
+
+//////////******************************************************************* */
+
+// string reversing by for loop 
+
+// let rev = '';
+// for (let i = 0; i < sentence.length; i++) {
+// //    console.log(i);
+// // console.log(sentence[i]);
+// const letter = sentence[i];
+// // console.log(letter);
+// rev = letter + rev ;
+    
+// }
+// console.log(rev);
+
+
+//////////******************************************************************* */
+
+// shortcut way to reverse
+const sentence = 'i love my country.';
+const reversed = sentence.split('').reverse().join(' ');
+console.log(reversed);
 
