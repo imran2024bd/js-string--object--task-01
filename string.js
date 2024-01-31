97d9131a9cb8dd5bd3f467f2a1266cb8ea69071a
imran2024bd <imranitbd34@gmail.com>
2024-01-31 11:45:26 +0600

    string task start

---
+++ string.js
@@ -0,0 +1,19 @@
+// starting string variable
+// Strings are for storing text
+// Strings are written with quotes , use single or double quotes:, 
+// Templates are strings enclosed in backticks (`This is a template string`).
+// Code	Result	Description
+// \'	'	Single quote
+// \"	"	Double quote
+// \\	\	Backslash
+
+
+const country = 'bangledesh';
+console.log(country);
+const address = '712 22 3';
+const capital = 'dhaka';
+const district = 'Natore';
+const sentence = 'i love my country';
+
+console.log(country.toUpperCase);
+
