67f6aeb249704d40a291044190415457cda47a73
imran2024bd <imranitbd34@gmail.com>
2024-01-31 13:45:58 +0600

    commit objects

---
+++ object.js
@@ -0,0 +1,31 @@
+//primitive
+// Basics
+const age = 21;
+const school = 'gouripur';
+const isPassed = true;
+// let isDeveloper;
+// console.log(isDeveloper);
+const subjects = ['bangla', 'English' , 'Physics', 'Math'];
+
+// objects  variable
+//Non-primitive
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color : 'white',
+    isClean : false
+}
+
+const subject ={
+    name : 'biology',
+    teacher : 'mukter',
+    examDate:'30 Feb',
+    chapters: [ 'first' , 'second','third'],
+    exams :{
+marks: 100;
+time: 180 ;
+    }
+}
+
+
+
