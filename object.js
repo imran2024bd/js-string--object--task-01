83af7a20b86191f5418924c11aed58ebeb917e82
imran2024bd <imranitbd34@gmail.com>
2024-01-31 21:00:24 +0600

    Keys, Values, Nested Objects And Delete task complete

--- object.js
+++ object.js
@@ -80,5 +80,45 @@ monitor: 'hp'
 // console.log(keys);
 
 // to find the values in a string
-const values = Object.values(computer);
-console.log(values);
+// const values = Object.values(computer);
+// console.log(values);
+
+// nested
+
+const college ={
+    name: 'VNC',
+    class: ['9','10','11', '12'],
+    events: ['science fair', 'bijoy dibos', '21 Feb'],
+    unique:{
+        color: 'blue',
+        result:{
+            gpa: 5,
+            merit: 'top'
+        }
+    }
+
+}
+// to find name
+// console.log(college.name);
+// to find unique
+// console.log(college.unique);
+// to find color
+// console.log(college.unique.color);
+// to find gpa
+// console.log(college.unique.result.gpa);
+
+// change value of merit in college.unique.result
+college.unique.result.merit = 'toper';
+// console.log(college);
+
+// to find bijoy dibos in events
+console.log(college.events[1]);
+
+// to change bijoy dibos in events
+college.events[1] = '16 December';
+console.log(college.events[1]);
+
+// to delete class 
+delete college.class;
+console.log(college);
+
