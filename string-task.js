19083ce61ff3224d80495123090e245d87717313
imran2024bd <imranitbd34@gmail.com>
2024-02-01 20:24:37 +0600

    Reverse a string using for loop

--- string-task.js
+++ string-task.js
@@ -1,4 +1,14 @@
 // What will the indexOf() method return for the following code?
 
-let str = "Mississippi";
-console.log(str.indexOf("i", 3));
+// let str = "Mississippi";
+// console.log(str.indexOf("i", 3));
+
+
+// Reverse a string using for loop
+
+var sentence = 'Reverse a string using for loop';
+
+for (let i = 0; i < sentence.length; i++) {
+    console.log(sentence[i]);
+    
+}
